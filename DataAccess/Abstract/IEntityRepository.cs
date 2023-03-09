a80f1294d79aa9d5b5d3dcfc122b41659e0b9e00
furkaydin <furkay16@gmail.com>
2023-03-09 23:38:11 +0300

    entity framework

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // IEntity bir interface olduğu için newlenemez. Burada new(), ekleyerek sadece IEntityden implement edilmiş somut bir yapıyı kullanabilir.
+    public interface IEntityRepository<T> where T:class, IEntity,new()  // hem referans tip olmasını istedik hemde IEntity implemente eden bir nesne olabilir. 
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EFProductDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EFProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            // using bağlantısı ile nesne ömrü yönetimi yapılır.(bellek yönetimini kontrol altına alır.)
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity); // ürünün referansını yakalar.
+                addedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Added; // Nesnenin veritabanına ekleneceğini belirtir.
+                context.SaveChanges(); // kaydet
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity); // referansı yakala
+                deletedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Deleted; // durumu silme olarak set et.
+                context.SaveChanges(); // kaydet
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var uptadedEntity = context.Entry(entity); // referansı yakala
+                uptadedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Modified; // durumu güncelleme olarak set et.
+                context.SaveChanges(); // kaydet
+            }
+        }
+    }
+}
