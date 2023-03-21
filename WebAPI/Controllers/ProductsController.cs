57a8f97988ec4bfe085e4566d6274a9ab96c63d5
furkaydin <furkay16@gmail.com>
2023-03-21 19:17:18 +0300

    product controller

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -23,11 +23,29 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll() // Http isteği döndürmek için customize edilen bir interface türü olan ActionResult. 
         {
             var result = _productService.GetAll();
-            return result.Data;
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+
+        public IActionResult Get(int id)
+        {
+            var result = _productService.GetById(id);
+            return result.Success ? Ok(result.Data) : BadRequest(result.Message);
         }
     }
 }
