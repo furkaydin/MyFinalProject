365d5cb3e101fa71f0b38273a4457feb627938ea
furkaydin <furkay16@gmail.com>
2023-03-22 15:57:13 +0300

    aop validation aspect

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -51,12 +52,12 @@ namespace Business.Concrete
         }
 
        
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             //business code
             // validation
 
-            ValidationTool.Validate(new ProductValidator(),product);
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded); 
         }
