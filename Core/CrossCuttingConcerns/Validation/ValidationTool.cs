6e9437037abd3adf450010d4832500893e834a76
furkaydin <furkay16@gmail.com>
2023-03-22 14:56:40 +0300

    product validator complete.

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool  // static bir sınıfın metodlarıda static olmalıdır.
+    {
+    public static void Validate(IValidator validator,object entity)
+        {
+            var context = new ValidationContext<object>(entity); // product validet edileceği seçiliyor.
+            var result = validator.Validate(context); // oluşturduğum nesneyi validate ederken contexti gönderiyorum içine.
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);  // Validation hatalarını döndürdü.
+            }
+        }
+    }
+}
